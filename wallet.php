ba72e003f63adf84174eeaf88ed8c48ac13210a3
Patel <nehal@itspatel.com>
2013-06-09 20:00:26 -0500

    Added BTC support to wallet.php

--- wallet.php
+++ wallet.php
@@ -4,17 +4,38 @@
 	
 	$wallet = array(
 		"Name" => "",
-		"Address" => "LMFFF1niJJeF3AnvNwFKnUKChCyYYHgqy2"
+		"Address" => "",
+		"Currency" => ""
 	);
 	
 	if (empty($wallet["Address"])) {
 		die("You didn't configure the wallet address.");
 	}
 	
-	$wallet_data = json_decode(file_get_contents("http://api.ltcd.info/address/" . $wallet["Address"]), true);
-	
-	if (isset($wallet_data["error"])) {
-		die("Error fetching wallet data: " . $wallet_data["error"]);
+	if ($wallet["Currency"] == "LTC") {
+		$wallet_data = json_decode(file_get_contents("http://api.ltcd.info/address/" . $wallet["Address"]), true);
+		if (isset($wallet_data["error"])) {
+			die("Error fetching wallet data: " . $wallet_data["error"]);
+		}
+		$transactions_key = "transactions";
+		$transactions_count = $wallet_data["txin"] + $wallet_data["txout"];
+		$block_key = "block";
+		$time_key = "timestamp";
+		$hash_url = "http://explorer.litecoin.net/tx/";
+		$amount_key = "amount";
+		$balance_key = "balance";
+	} elseif ($wallet["Currency"] == "BTC") {
+		$wallet_data = json_decode(@file_get_contents("http://blockchain.info/address/" . $wallet["Address"] . "?format=json"), true);
+		if (!$wallet_data) {
+			die("Error fetching wallet data");
+		}
+		$transactions_key = "txs";
+		$transactions_count = $wallet_data["n_tx"];
+		$block_key = "block_height";
+		$time_key = "time";
+		$hash_url = "http://blockchain.info/tx/";
+		$amount_key = "result";
+		$balance_key = "final_balance";
 	}
 	
 ?>
@@ -41,11 +62,11 @@
 			<div class="well well-small info-block">
 				<strong>Address:</strong> <span class="break-word"><?php echo $wallet["Address"]; ?></span><?php echo PHP_EOL; ?>
 				<hr style="margin-top: 5px; margin-bottom: 5px;">
-				<strong>Balance:</strong> <?php echo $wallet_data["balance"] . " LTC" . PHP_EOL; ?>
+				<strong>Balance:</strong> <?php if ($wallet["Currency"] == "LTC") { echo $wallet_data[$balance_key] . " " . $wallet["Currency"];  } else { echo number_format($wallet_data[$balance_key] / 100000000, 8) . " " . $wallet["Currency"]; } echo PHP_EOL; ?>
 				<br>
-				<strong>Transactions:</strong> <?php echo $wallet_data["txin"] . " in, " . $wallet_data["txout"] . " out"; ?>
+				<strong>Transactions:</strong> <?php echo $transactions_count; ?>
 			</div>
-			<?php if (count($wallet_data["transactions"]) > 0) { ?>
+			<?php if (count($wallet_data[$transactions_key]) > 0) { ?>
 			<h1 class="info-header">Transactions</h1>
 			<table class="table table-striped table-bordered table-hover info-block">
 				<thead>
@@ -59,19 +80,46 @@
 				</thead>
 				<tbody>
 <?php
-						function cmp($a, $b) {
-								return $b["timestamp"] - $a["timestamp"];
+						
+						function sort_chronologically($a, $b) {
+							global $time_key;
+							return $b[$time_key] - $a[$time_key];
 						}
-						usort($wallet_data["transactions"], "cmp");
-
-						foreach ($wallet_data["transactions"] as $id => $transaction) {
+						usort($wallet_data[$transactions_key], "sort_chronologically");
+						
+						foreach ($wallet_data[$transactions_key] as $id => $transaction) {
+							if ($wallet["Currency"] == "LTC") {
+								$amount = $transaction["amount"];
+							} elseif ($wallet["Currency"] == "BTC") {
+								$sent = 0;
+								foreach ($transaction["inputs"] as $input) {
+									if ($input["prev_out"]["addr"] == $wallet["Address"]) {
+										$sent += $input["prev_out"]["value"];
+									}
+								}
+								
+								$received = 0;
+								foreach ($transaction["out"] as $input) {
+									if ($input["addr"] == $wallet["Address"]) {
+										$received += $input["value"];
+									}
+								}
+								
+								if ($sent == 0) {
+									$amount = $received / 100000000;
+								} elseif ($received == 0) {
+									$amount = $sent / 100000000;
+									$amount = 0 - $amount;
+								}
+								$amount = number_format($amount, 8);
+							}
 					?>
-					<tr class="<?php if ($transaction["amount"] >= 0) { echo "success"; } else { echo "error"; } ?>">
-						<td data-title="ID"><?php echo count($wallet_data["transactions"]) - $id; ?></td>
-						<td data-title="Transaction" class="break-word"><a href="http://explorer.litecoin.net/tx/<?php echo $transaction["hash"]; ?>"><?php echo $transaction["hash"]; ?></a></td>
-						<td data-title="Block"><?php echo $transaction["block"]; ?></td>
-						<td data-title="Time"><?php $date_timezone = new DateTime(date("Y-m-d H:i:s", $transaction["timestamp"]), new DateTimeZone("UTC")); $date_timezone->setTimezone(new DateTimeZone($timezone)); echo $date_timezone->format('M j, Y h:i:s A'); ?></td>
-						<td data-title="Amount"><?php echo $transaction["amount"]; ?></td>
+					<tr class="<?php if ($amount >= 0) { echo "success"; } else { echo "error"; } ?>">
+						<td data-title="ID"><?php echo $transactions_count - $id; ?></td>
+						<td data-title="Transaction" class="break-word"><a href="<?php echo $hash_url . $transaction["hash"]; ?>"><?php echo $transaction["hash"]; ?></a></td>
+						<td data-title="Block"><?php echo $transaction[$block_key]; ?></td>
+						<td data-title="Time"><?php $date_timezone = new DateTime(date("Y-m-d H:i:s", $transaction[$time_key]), new DateTimeZone("UTC")); $date_timezone->setTimezone(new DateTimeZone($timezone)); echo $date_timezone->format('M j, Y h:i:s A'); ?></td>
+						<td data-title="Amount"><?php echo $amount; ?></td>
 					</tr>
 <?php
 						}
